409c2f8df6ace30efa1e3a79a6418878e906fc94
Jonathan Cohen <jcohen66g@gmail.com>
2022-12-30 11:39:33 -0500

    add error file

---
+++ errors.scala
@@ -0,0 +1,14 @@
+object PatternMatchingErrors extends App {
+    def patternMatching(variable: Any): String = {
+        // More specific cases must appear before less specific
+        // or they will never get a chance to match.
+        variable match {
+            case _: Seq[_] => "Variable is of type Sequence"
+            case _: List[_] => "Variable is of type List"
+            case _: Vector[_] => "Variable is of type Vector"
+        }
+    }
+    println(patternMatching(variable=List(1,2,3)))
+}
+
+
